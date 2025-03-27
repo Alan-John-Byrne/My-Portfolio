2562840ee00944adb4618a2a11d37e16b9b94a10
alanjohnbarnesbyrne@gmail.com <x00167885@mytudublin.ie>
2025-03-27 21:46:04 +0000

    refactor: Switching to ReactTS

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// NOTE: Creating root component to insert into 'index.html'.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)// DEF: 'StrictMode': (For entire app)
+//  Whilst in dev mode, all components....:
+//  1- Rerender an extra time.
+//  2- Rerun Effects an extra time.
+//  3- Rerun Ref callbacks an extra time.
+//  All to help find bugs quicker.
