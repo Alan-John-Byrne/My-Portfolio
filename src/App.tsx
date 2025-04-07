6241c18366b44e422036f38aa0aaa0c40b2c70db
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2025-04-07 17:47:59 +0100

    refactor: Redoing the portfolio using React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AlanPic from './assets/images/Alan Byrne - Original.jpg'
+
+function App() {
+  return (
+    <>
+      <h1>Vite + ReactTS Project</h1>
+      <img src={AlanPic} className="logo" alt="Vite logo" />
+    </>
+  );
+}
+
+export default App
